354fed7101115ba15ed4aa88ab6c5de9bcc2dc23
Phil Hagelberg <technomancy@gmail.com>
2011-09-06 14:15:39 -0700

    Switch to configuring with environment vars.

--- src/clojars/config.clj
+++ src/clojars/config.clj
@@ -1,8 +1,23 @@
-(ns clojars.config)
+(ns clojars.config
+  (:require [clojure.string :as str])
+  (:import (java.net URI)))
+
+(defn database-resource []
+  (let [url (URI. (or (System/getenv "DATABASE_URL")
+                      "//localhost:5432/clojars"))
+        host (.getHost url)
+        port (if (pos? (.getPort url)) (.getPort url) 5432)
+        path (.getPath url)]
+    (merge
+      {:subname (str "//" host ":" port path)}
+      (if-let [user-info (.getUserInfo url)]
+        {:user (first (str/split user-info #":"))
+         :password (second (str/split user-info #":"))}
+        {:user "clojars", :password "typhoons"}))))
 
 (def config
-  {:db {:classname "org.sqlite.JDBC"
-        :subprotocol "sqlite"
-        :subname "data/db"}
+  {:db (merge {:classname "org.postgresql.Driver"
+               :subprotocol "postgresql"}
+              (database-resource))
    :key-file "data/authorized_keys"
    :repo "repo"})
